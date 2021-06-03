8769a9541d9c88164ac629cc4a5be7080749392f
LorranPdS <lorransantospereira@yahoo.com.br>
2021-06-02 23:42:15 -0300

    Exemplos de concatenação

---
+++ 1AlgoritmoComputacional/4ConcatenacaoTexto.dart
@@ -0,0 +1,17 @@
+/** Aqui vamos concatenar textos usando o sinal de + */
+/* Exemplo 1 
+main() {
+  var total = "Hello " + "World";
+  print(total);
+}
+*/
+
+/** Exemplo 2 */
+main() {
+  var texto1 = "Missão ";
+  var texto2 = "Flutter";
+  
+  var total = texto1 + texto2;
+
+  print(total);
+}
